0f3af0178a6ce077e21766eb44978765c52074b0
Nakorn Sinpadung <nakorndev@gmail.com>
2021-08-19 21:20:44 +0700

    Build page

---
+++ precache-manifest.75131e5100ef6e07992d361b910f999e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e35e47726c4fdbcb0db68fe81bf1162",
+    "url": "/poptooclick/android-chrome-192x192.png"
+  },
+  {
+    "revision": "a77f433aa9c63ac3469ea084408d67b2",
+    "url": "/poptooclick/android-chrome-512x512.png"
+  },
+  {
+    "revision": "de6d71c4a0bcde5ef93ce318e3adf670",
+    "url": "/poptooclick/apple-touch-icon.png"
+  },
+  {
+    "revision": "45e8bcb9c13b049feaf1",
+    "url": "/poptooclick/css/app.79ce7ef6.css"
+  },
+  {
+    "revision": "a8e5e83d3ea9f7bd419f4aead96fa69d",
+    "url": "/poptooclick/favicon-16x16.png"
+  },
+  {
+    "revision": "dd485b26d3ed7f4841514340cd557f68",
+    "url": "/poptooclick/favicon-32x32.png"
+  },
+  {
+    "revision": "042ee8eacd02fb40255bc8975baac47a",
+    "url": "/poptooclick/images/angry-mini.png"
+  },
+  {
+    "revision": "8ae24bf0a723a9a3e7ea6882873de4af",
+    "url": "/poptooclick/images/angry.png"
+  },
+  {
+    "revision": "31fd6ace8bbada034c0a608d2841c0a7",
+    "url": "/poptooclick/images/love-mini.png"
+  },
+  {
+    "revision": "24060621085cb9b5ce3b28a25daadac8",
+    "url": "/poptooclick/images/love.png"
+  },
+  {
+    "revision": "124fa3f803fcc30f26f436e589edca4b",
+    "url": "/poptooclick/images/tony-approved-it-is-hack.png"
+  },
+  {
+    "revision": "684a5de5ea44dbddb2a543dcf121eab8",
+    "url": "/poptooclick/index.html"
+  },
+  {
+    "revision": "45e8bcb9c13b049feaf1",
+    "url": "/poptooclick/js/app.b6183c2d.js"
+  },
+  {
+    "revision": "7cccc57402d03a2a29e3",
+    "url": "/poptooclick/js/chunk-vendors.17ce810d.js"
+  },
+  {
+    "revision": "155ce853fb9f528644ec1a0f7eed95f3",
+    "url": "/poptooclick/manifest.json"
+  },
+  {
+    "revision": "8b865ac9ac0e278db22739ca2aa5f1bd",
+    "url": "/poptooclick/pop.mp3"
+  },
+  {
+    "revision": "733c79313f109d07e301c8236efb0071",
+    "url": "/poptooclick/preview.png"
+  },
+  {
+    "revision": "6f94032faf83ae58741013c8fee07cb3",
+    "url": "/poptooclick/robots.txt"
+  },
+  {
+    "revision": "9669b289571975b822163cf69e36745b",
+    "url": "/poptooclick/site.webmanifest"
+  }
+]);
